ba6ce26c4ee83caff7051e57ae59a06bd0d66cc1
NCHU CVML <39890539+nchucvml@users.noreply.github.com>
2021-08-12 14:52:53 +0800

    Add files via upload

---
+++ datasets/convert_data.py
@@ -0,0 +1,191 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from datasets import dataset_utils
+
+import tensorflow as tf
+import random
+import math
+import sys
+import os
+
+# The URL where the tensorflow data can be downloaded.
+_DATA_URL = 'http://download.tensorflow.org/example_images/xxx.tgz'
+
+
+# Seed for repeatability.
+_RANDOM_SEED = 0
+
+# The number of shards per dataset split.
+_NUM_SHARDS = 1
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+class ImageReader(object):
+    """Helper class that provides TensorFlow image coding utilities."""
+
+    def __init__(self):
+        # Initializes function that decodes RGB JPEG data.
+        self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
+        self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
+
+    def read_image_dims(self, sess, image_data):
+        image = self.decode_jpeg(sess, image_data)
+        return image.shape[0], image.shape[1]
+
+    def decode_jpeg(self, sess, image_data):
+        image = sess.run(self._decode_jpeg,
+                         feed_dict={self._decode_jpeg_data: image_data})
+        assert len(image.shape) == 3
+        assert image.shape[2] == 3
+        return image
+
+
+def _get_filenames_and_classes(dataset_dir):
+    """Returns a list of filenames and inferred class names.
+
+    Args:
+        dataset_dir: A directory containing a set of subdirectories representing
+        class names. Each subdirectory should contain PNG or JPG encoded images.
+
+    Returns:
+        A list of image file paths, relative to `dataset_dir` and the list of
+        subdirectories, representing class names.
+    """
+    images_root = dataset_dir
+    directories = []
+    class_names = []
+    for filename in os.listdir(images_root):
+        path = os.path.join(images_root, filename)
+        if os.path.isdir(path):
+            directories.append(path)
+            class_names.append(filename)
+
+    photo_filenames = []
+    for directory in directories:
+        for filename in os.listdir(directory):
+            path = os.path.join(directory, filename)
+            photo_filenames.append(path)
+
+    return photo_filenames, sorted(class_names)
+
+
+def _get_dataset_filename(dataset_dir, split_name, shard_id):
+    output_filename = 'data_%s_%05d-of-%05d.tfrecord' % (
+        split_name, shard_id, _NUM_SHARDS)
+    return os.path.join(dataset_dir, output_filename)
+
+
+def _convert_dataset(split_name, filenames, class_names_to_ids, dataset_dir):
+    """Converts the given filenames to a TFRecord dataset.
+
+    Args:
+        split_name: The name of the dataset, either 'train' or 'validation'.
+        filenames: A list of absolute paths to png or jpg images.
+        class_names_to_ids: A dictionary from class names (strings) to ids
+        (integers).
+    dataset_dir: The directory where the converted datasets are stored.
+    """
+    assert split_name in ['train', 'validation']
+
+    num_per_shard = int(math.ceil(len(filenames) / float(_NUM_SHARDS)))
+
+    with tf.Graph().as_default():
+        image_reader = ImageReader()
+
+        with tf.Session('') as sess:
+
+            for shard_id in range(_NUM_SHARDS):
+                output_filename = _get_dataset_filename(
+                    dataset_dir, split_name, shard_id)
+
+                with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
+                    start_ndx = shard_id * num_per_shard
+                    end_ndx = min((shard_id + 1) * num_per_shard, len(filenames))
+                    for i in range(start_ndx, end_ndx):
+                        sys.stdout.write('\r>> Converting image %d/%d shard %d' % (
+                            i + 1, len(filenames), shard_id))
+                        sys.stdout.flush()
+
+                        # Read the filename:
+                        image_data = tf.gfile.FastGFile(filenames[i], 'rb').read()
+                        height, width = image_reader.read_image_dims(sess, image_data)
+
+                        class_name = os.path.basename(os.path.dirname(filenames[i]))
+                        class_name2 = os.path.split(os.path.abspath(filenames[i]))[1]
+                        print('')
+                        print('class: ', class_name)
+                        print('file name: ', class_name2)
+                        class_name2 = tf.compat.as_bytes(class_name2)
+                        class_id = class_names_to_ids[class_name]
+                        class_name2 = tf.train.Feature(bytes_list=tf.train.BytesList(value=[class_name2]))
+                        example = dataset_utils.image_to_tfexample(
+                            image_data, b'png', height, width, class_id, filenames[i])
+                        tfrecord_writer.write(example.SerializeToString())
+
+    sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+def _clean_up_temporary_files(dataset_dir):
+    """Removes temporary files used to create the dataset.
+
+    Args:
+        dataset_dir: The directory where the temporary files are stored.
+    """
+    filename = _DATA_URL.split('/')[-1]
+    filepath = os.path.join(dataset_dir, filename)
+    tf.gfile.Remove(filepath)
+
+    tmp_dir = os.path.join(dataset_dir, 'images_photos')
+    tf.gfile.DeleteRecursively(tmp_dir)
+
+
+def _dataset_exists(dataset_dir):
+    for split_name in ['train', 'validation']:
+        for shard_id in range(_NUM_SHARDS):
+            output_filename = _get_dataset_filename(
+                dataset_dir, split_name, shard_id)
+            if not tf.gfile.Exists(output_filename):
+                return False
+    return True
+
+
+def run(dataset_source_train_dir, dataset_source_test_dir, dataset_destinaiton_dir):
+    """Runs the download and conversion operation.
+
+    Args:
+        dataset_destinaiton_dir:
+        dataset_source_train_dir: The dataset directory where the dataset is stored.
+        dataset_split_test: The dataset split for testing.
+    """
+    if _dataset_exists(dataset_source_train_dir):
+        print('Dataset files already exist. Exiting without re-creating them.')
+        return
+
+    photo_filenames_tr, class_names_tr = _get_filenames_and_classes(dataset_source_train_dir)
+    photo_filenames_te, class_names_te = _get_filenames_and_classes(dataset_source_test_dir)
+    class_names_to_ids = dict(zip(class_names_tr, range(len(class_names_tr))))
+
+    # Divide into train and test:
+    random.seed(_RANDOM_SEED)
+    random.shuffle(photo_filenames_tr)
+    training_filenames = photo_filenames_tr
+    validation_filenames = photo_filenames_te
+
+    # First, convert the training and validation sets.
+    _convert_dataset('train', training_filenames, class_names_to_ids, dataset_destinaiton_dir)
+    _convert_dataset('validation', validation_filenames, class_names_to_ids, dataset_destinaiton_dir)
+
+    # Finally, write the labels file:
+    labels_to_class_names = dict(zip(range(len(class_names_tr)), class_names_tr))
+    dataset_utils.write_label_file(labels_to_class_names, dataset_destinaiton_dir)
+
+    print('\nFinished converting the images dataset!')
